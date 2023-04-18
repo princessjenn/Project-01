331f179dec45f25800cb2bc8b9f891353b08a9a7
kerperryjr <keriperryjr@gmail.com>
2023-04-18 11:40:48 -0500

    added translator

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ fetch("https://api.kanye.rest")
     document.body.appendChild(quote);
   });
 
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("source_language", "en");
+  encodedParams.append("target_language", "id");
+  encodedParams.append("text", "");
+  
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': '594f24e57fmshfcd896d19759eb4p17bf70jsn8317974053d7',
+      'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    },
+    body: encodedParams
+  };
+  
+
+  fetch('https://text-translator2.p.rapidapi.com/translate', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
